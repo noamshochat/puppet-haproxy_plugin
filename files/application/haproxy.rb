6aaa55e9287ec599ce4d229283b0c0da9d882ae5
alonbecker <alon@alonbecker.com>
2013-10-28 02:38:53 +0200

    Fixed application

--- files/application/haproxy.rb
+++ files/application/haproxy.rb
@@ -1,12 +1,44 @@
 class MCollective::Application::Haproxy<MCollective::Application
-  description "Reports on usage for a specific fact"
+  def post_option_parser(configuration)
+    if ARGV.length >= 1
+      configuration[:command] = ARGV.shift
+    else
+      docs
+    end
+  end
 
+  def docs
+    puts "Usage: #{$0} [enable|disable|status"
+  end
 
   def main
-    mc = rpcclient("haproxy")
-
-    printrpc mc.haproxy(:options => options)
-
+    mc = rpcclient("haproxy", :chomp => true)
+    mc.send(configuration[:command], options).each do |resp|
+      puts "#{resp[:sender]}:"
+      if resp[:statuscode] == 0
+        responses, statuses = parse_lines(resp[:data][:output])
+        puts responses if responses and ['enable','disable'].include? configuration[:command]
+        puts statuses if statuses and configuration[:command] == 'status'
+      else
+        puts resp[:statusmsg]
+      end
+    end
+    mc.disconnect
     printrpcstats
+  end
+
+  def parse_lines(output)
+    responses = ""
+    statuses = ""
+    output.each_line do |line|
+      case line
+        when /^\w+:/
+          responses = responses + (sprintf '%-30s', "    #{line}")
+        when /^\w+,/
+          fields = line.split(',')
+          statuses = statuses + (sprintf '%-30s %s', "    #{fields[0]}/#{fields[1]}:", "#{fields[17]}\n")
+      end
+    end
+    [responses, statuses]
   end
 end
